a08f8a0f56d93e137c34ef1181d65aa922838ae0
jzf21 <jozefneelamkavil2003@gmail.com>
2023-06-09 20:27:58 +0530

    added card

--- apps/web-client/src/pages/scanner.tsx
+++ apps/web-client/src/pages/scanner.tsx
@@ -1,4 +1,5 @@
 import Navbar from '@/Components/Navbar';
+import ParticipantCard from '@/Components/ParticipantCard';
 import Scanner from '@/Components/Scanner';
 import { Button, Flex, color } from '@chakra-ui/react';
 import React, { useState } from 'react';
@@ -9,16 +10,28 @@ const ScannerComponent: React.FC<Props> = () => {
   const [participant, setParticipant] = useState<string>('');
 
   return (
-  <>
-    <Navbar/>
-    <Flex align={'center'} direction={'column'} gap={4} justify={'center'} h={'100vh'} w={'100vw'}>
-      <Scanner setResult={(result: string) => setParticipant(result)} />
-      <p style={{ color: 'black' }}>{participant}</p>
-      <Button
-        onClick={() => {
-          console.log(participant);
-        }}>Checkin</Button>
-    </Flex></>
+    <>
+      <Navbar />
+      <Flex
+        align={'center'}
+        direction={'column'}
+        gap={4}
+        justify={'center'}
+        h={'100vh'}
+        w={'100vw'}
+      >
+        <Scanner setResult={(result: string) => setParticipant(result)} />
+        <p style={{ color: 'black' }}>{participant}</p>
+        <Button
+          onClick={() => {
+            console.log(participant);
+          }}
+        >
+          Checkin
+        </Button>
+        <ParticipantCard />
+      </Flex>
+    </>
   );
 };
 
