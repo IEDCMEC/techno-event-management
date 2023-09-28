325171daf4197749c3d855fadcbb84cad4a8947c
Allen Shibu <93600615+alllenshibu@users.noreply.github.com>
2023-09-28 20:23:04 +0530

    Update Scanner.tsx

--- apps/web-client/src/components/Scanner/Scanner.tsx
+++ apps/web-client/src/components/Scanner/Scanner.tsx
@@ -1,41 +1,15 @@
-import React, { useState } from 'react';
-// import QrReader from 'react-qr-reader';
-import dynamic from 'next/dynamic';
-import styles from './Scanner.module.css';
-
-const QrReader = dynamic(() => import('react-qr-reader'), {
-  ssr: false,
-});
+import React from 'react';
+import { QrReader } from 'react-qr-reader';
 
 interface ScannerProps {
-  qr_pay: boolean;
-  setUserId: (userId: string) => void;
-  userId: string;
-  setPaymentId: (paymentId: string) => void;
-  paymentId: string;
+  result: String;
+  setResult: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export const Scanner: React.FC<ScannerProps> = ({
-  qr_pay,
-  setUserId,
-  userId,
-  setPaymentId,
-  paymentId,
-}) => {
-  const [result, setResult] = useState<string | null>('');
-
+const Scanner: React.FC<ScannerProps> = ({ result, setResult }) => {
   const handleScan = (result: string | null) => {
     console.log(result);
     setResult(result || '');
-    if (qr_pay) {
-      if (result !== null) {
-        setPaymentId(result);
-      }
-    } else {
-      if (result !== null) {
-        setUserId(result);
-      }
-    }
   };
 
   const handleError = (err: Error) => {
@@ -43,27 +17,10 @@ export const Scanner: React.FC<ScannerProps> = ({
   };
 
   return (
-    <div className={styles.scan_container}>
-      <div className={styles.scan}>
-        <div className={styles.scan_heading}>Scan the {qr_pay ? 'Payment' : 'Band'} QR code</div>
-        <QrReader onScan={handleScan} onError={handleError} />
-      </div>
-
-      <div className={styles.text_input}>
-        <input
-          className={styles.input}
-          type="text"
-          placeholder="Enter code"
-          value={qr_pay ? paymentId : userId}
-          onChange={(e) => {
-            if (qr_pay) {
-              setPaymentId(e.target.value);
-            } else {
-              setUserId(e.target.value);
-            }
-          }}
-        />
-      </div>
+    <div>
+      <QrReader onResult={handleScan} onError={handleError} />
     </div>
   );
 };
+
+export default Scanner;
