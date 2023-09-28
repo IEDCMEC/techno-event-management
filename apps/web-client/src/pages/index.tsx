e84c570e230c1a29eb7652d52fa38e92da7b58ff
Allen Shibu <93600615+alllenshibu@users.noreply.github.com>
2023-09-28 20:23:46 +0530

    Update index.tsx

--- apps/web-client/src/pages/index.tsx
+++ apps/web-client/src/pages/index.tsx
@@ -1,25 +1,16 @@
-import { Scanner } from '@/components/Scanner/Scanner';
+import Scanner from '@/components/Scanner/Scanner';
 
 import { useState } from 'react';
 
 const Home = () => {
-  const [data, setData] = useState('No result');
+  const [result, setResult] = useState('No result');
 
   return (
     <main>
       <h1>Web Client</h1>
 
-      <Scanner
-        qr_pay={false} // Example props, adjust as needed
-        setUserId={(userId) => {
-          // Implement your setUserId logic here
-        }}
-        userId=""
-        setPaymentId={(paymentId) => {
-          // Implement your setPaymentId logic here
-        }}
-        paymentId=""
-      />
+      <Scanner result={result} setResult={setResult} />
+      {result}
     </main>
   );
 };
