d775040afdc6fe4a139e283c69cbd622b17b1da7
jzf21 <jozefneelamkavil2003@gmail.com>
2023-09-29 22:20:33 +0530

    reader fixed?

--- apps/web-client/src/types/scanner.d.ts
+++ apps/web-client/src/types/scanner.d.ts
@@ -2,11 +2,11 @@ declare module 'react-qr-reader' {
   import * as React from 'react';
 
   interface QrReaderProps {
-    onScan: (data: string | null) => void;
+    onResult: (data: string | null) => void;
     onError: (error: Error) => void;
     // Add other props as needed
   }
 
-  const QrReader: React.FC<QrReaderProps>;
-  export default QrReader;
+  export const QrReader: React.FC<QrReaderProps>;
+  
 }
