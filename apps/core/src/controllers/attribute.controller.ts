d09d712857b2cd875889d0ea54070351b673dc64
Allen Shibu <93600615+alllenshibu@users.noreply.github.com>
2023-10-10 18:56:47 +0530

    feat(web-client): Add basic participant attribute functionality (#112)

--- apps/core/src/controllers/attribute.controller.ts
+++ apps/core/src/controllers/attribute.controller.ts
@@ -1,9 +1,38 @@
-const getAllEventAttributes = () => {
-  return { message: 'Get all attributes' };
+import { Request, Response } from 'express';
+import { AttributeService } from '../services/attribute.service';
+
+const attributeController = (attributeService: AttributeService) => {
+  return {
+    addNewAttributeController: async (req: Request, res: Response) => {
+      try {
+        const organizationId = req?.params?.organizationId;
+        const eventId = req?.params?.eventId;
+        const name = req?.body?.attribute?.name;
+
+        if (!organizationId || organizationId === '' || organizationId === undefined) {
+          return res.status(400).json({ error: 'Organization ID is required' });
+        }
+
+        if (!eventId || eventId === '' || eventId === undefined) {
+          return res.status(400).json({ error: 'Event ID is required' });
+        }
+
+        if (!name || name === '' || name === undefined) {
+          return res.status(400).json({ error: 'Attribute name is required' });
+        }
+
+        const newAttribute = await attributeService().addNewAttributeService(
+          organizationId,
+          eventId,
+          name,
+        );
+
+        return res.status(201).json({ attribute: newAttribute });
+      } catch (err) {
+        return res.status(500).json({ error: err.message });
+      }
+    },
+  };
 };
 
-const getEventAttributeById = () => {
-  return { message: 'Get attribute by id' };
-};
-
-export { getAllEventAttributes, getEventAttributeById };
+export default attributeController;
--- apps/core/src/services/attribute.service.ts
+++ apps/core/src/services/attribute.service.ts
@@ -1,7 +1,37 @@
-const getEventAttributes = async (organizationId: string, eventId: string) => {};
+const pg = require('pgdatabase').pg;
 
-const getEventAttribute = async (
-  organizationId: string,
-  eventId: string,
-  attributeId: string,
-) => {};
+type AttributeService = {
+    addNewAttributeService: (
+        organizationId: string,
+        eventId: string,
+        name: string,
+    ) => Promise<any>;
+};
+
+const attributeService = (): AttributeService => {
+    return {
+        addNewAttributeService: async (
+            organizationId: string,
+            eventId: string,
+            name: string,
+        ) => {
+            try {
+                let newAttribute = (
+                    await pg.query(
+                        `INSERT INTO attribute(organization_id, event_id, name) VALUES($1, $2, $3) RETURNING *`,
+                        [organizationId, eventId, name],
+                    )
+                ).rows[0];
+
+                if (!newAttribute) throw new Error('Something went wrong');
+
+                return newAttribute;
+            } catch (err: any) {
+                console.error(err);
+                throw new Error('Something went wrong');
+            }
+        },
+    };
+};
+export default attributeService
+export type {AttributeService};
