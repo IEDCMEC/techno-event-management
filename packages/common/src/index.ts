995da1d4858c68be9b0197244840f514083ecb52
Allen Shibu <93600615+alllenshibu@users.noreply.github.com>
2023-11-06 00:43:00 +0530

    fix: Fix types error (#143)

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,12 +1,12 @@
 import Role from './enums/Role';
 import Subscription from './enums/Subscription';
 
-import Attribute from './types/Attribute';
-import Event from './types/Event';
-import Extra from './types/Extra';
-import Organization from './types/Organization';
-import Participant from './types/Participant';
-import User from './types/User';
+import type Attribute from './types/Attribute';
+import type Event from './types/Event';
+import type Extra from './types/Extra';
+import type Organization from './types/Organization';
+import type Participant from './types/Participant';
+import type User from './types/User';
 
 export { Role, Subscription };
 export type { Attribute, Event, Extra, Organization, Participant, User };
