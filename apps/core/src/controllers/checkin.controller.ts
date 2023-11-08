c74f62d29b8eb39bb2989d980523a5a7e14bbc68
Allen Shibu <93600615+alllenshibu@users.noreply.github.com>
2023-11-08 21:20:49 +0530

    feat: Complete participant checkin (#156)

--- apps/core/src/controllers/checkin.controller.ts
+++ apps/core/src/controllers/checkin.controller.ts
@@ -34,7 +34,37 @@ const checkinController = (checkinService: CheckinService) => {
           checkinTime,
           user.id,
         );
-        return res.status(201).json({ newParticipantCheckin });
+        return res.status(200).json({ newParticipantCheckin });
+      } catch (err) {
+        return res.status(500).json({ error: 'Something went wrong' });
+      }
+    },
+    getParticipantCheckinDetailsController: async (req: Request, res: Response) => {
+      try {
+        const user = req?.body?.user;
+        const organizationId = req?.params?.organizationId;
+        const eventId = req?.params?.eventId;
+        const participantId = req?.params?.participantId;
+
+        if (!organizationId || organizationId === '' || organizationId === undefined) {
+          return res.status(400).json({ error: 'Organization ID is required' });
+        }
+
+        if (!eventId || eventId === '' || eventId === undefined) {
+          return res.status(400).json({ error: 'Event ID is required' });
+        }
+
+        if (!participantId || participantId === '' || participantId === undefined) {
+          return res.status(400).json({ error: 'Participant ID is required' });
+        }
+
+        const participantCheckinDetails =
+          await checkinService().getParticipantCheckinDetailsService(
+            organizationId,
+            eventId,
+            participantId,
+          );
+        return res.status(200).json({ participantCheckinDetails });
       } catch (err) {
         return res.status(500).json({ error: 'Something went wrong' });
       }
