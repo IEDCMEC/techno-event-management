8ed615f46faa1a583980d1092e7046942828bf55
adilmp1 <115640364+adilmp1@users.noreply.github.com>
2024-02-03 21:47:57 +0530

    Updated events

--- apps/web-admin/src/pages/organizations/[orgId]/events/index.jsx
+++ apps/web-admin/src/pages/organizations/[orgId]/events/index.jsx
@@ -13,6 +13,7 @@ import {
   Tr,
   TableContainer,
   Text,
+  Button,
 } from '@chakra-ui/react';
 
 import { useFetch } from '@/hooks/useFetch';
@@ -28,7 +29,9 @@ export default function Events() {
   const { loading, get } = useFetch();
 
   const [events, setEvents] = useState([]);
-
+  const handleClick = () => {
+    router.push(`/organizations/${orgId}/events/new/`);
+  };
   useEffect(() => {
     const fetchEvents = async () => {
       const { data, status } = await get(`/core/organizations/${orgId}/events`);
@@ -48,10 +51,20 @@ export default function Events() {
         justifyContent="center"
         gap={8}
       >
-        <Box width="100%" p={8}>
+        <Box width="100%" p={8} display="flex" justifyContent="space-between">
           <Text fontSize="4xl" fontWeight="bold">
             Events
           </Text>
+          <Button
+            padding="4"
+            minWidth="-moz-initial"
+            bgColor="rgb(128, 90, 213)"
+            color="white"
+            _hover={{ bgColor: 'rgb(100, 70, 183)' }}
+            onClick={handleClick}
+          >
+            Add Event
+          </Button>
         </Box>
         <Box width="100%" height="100%">
           <TableContainer width="100%" height="100%">
