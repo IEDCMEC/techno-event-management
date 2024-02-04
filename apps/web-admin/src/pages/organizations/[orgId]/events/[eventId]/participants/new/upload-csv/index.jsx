4ed4f59bf405b4cea71bccbd895e686a198667e0
Jaison Dennis <jaisondennis080@gmail.com>
2024-02-04 22:36:30 +0530

    Added CSV Parser and DataGrd (#272)
    Co-authored-by: Allen Shibu <93600615+alllenshibu@users.noreply.github.com>

---
+++ apps/web-admin/src/pages/organizations/[orgId]/events/[eventId]/participants/new/upload-csv/index.jsx
@@ -0,0 +1,75 @@
+import { useState } from 'react';
+import Papa from 'papaparse';
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
+
+import { Flex } from '@chakra-ui/react';
+
+import DashboardLayout from '@/layouts/DashboardLayout';
+import { ThemeProvider, createTheme } from '@mui/material';
+
+const MuiTheme = createTheme({});
+
+export default function NewOrganization() {
+  const [csvData, setCSVData] = useState(null);
+
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+
+    Papa.parse(file, {
+      header: true,
+      dynamicTyping: true,
+      complete: (result) => {
+        const dataWithId = result.data.map((row, index) => ({ ...row, id: index + 1 }));
+        setCSVData(dataWithId);
+        console.log('CSV Data as JSON:', dataWithId);
+      },
+      error: (error) => {
+        console.error('Error parsing CSV:', error);
+      },
+    });
+  };
+
+  const columns = [
+    { field: 'FirstName', headerName: 'First Name', width: 150 },
+    { field: 'LastName', headerName: 'Last Name', width: 150 },
+    { field: 'Email', headerName: 'Email', width: 150 },
+    { field: 'PhoneNumber', headerName: 'Phone Number', width: 150 },
+  ];
+
+  return (
+    <DashboardLayout>
+      <Flex
+        direction="column"
+        height="100%"
+        width="100%"
+        alignItems="center"
+        justifyContent="center"
+        gap={8}
+      >
+        <div>
+          <h1>CSV Uploader</h1>
+          <input type="file" accept=".csv" onChange={handleFileUpload} />
+          {csvData && (
+            <ThemeProvider theme={MuiTheme}>
+              <DataGrid
+                rows={csvData}
+                columns={columns}
+                components={{
+                  Toolbar: GridToolbar,
+                }}
+                componentsProps={{
+                  toolbar: {
+                    showQuickFilter: true,
+
+                    quickFilterProps: { debounceMs: 500 },
+                  },
+                }}
+                autoHeight
+              />
+            </ThemeProvider>
+          )}
+        </div>
+      </Flex>
+    </DashboardLayout>
+  );
+}
