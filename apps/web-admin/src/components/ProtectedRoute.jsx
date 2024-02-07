324d4ec34e239898cbab829473677e6032bf0c87
Riya Sunil <riaelisabeths@gmail.com>
2024-02-08 00:19:06 +0530

    fix(web-admin): Add loading screen in protected routes (#285)

--- apps/web-admin/src/components/ProtectedRoute.jsx
+++ apps/web-admin/src/components/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useAuth0 } from '@auth0/auth0-react';
-
+import { Spinner, VStack, AbsoluteCenter } from '@chakra-ui/react';
 export const ProtectedRoute = ({ children }) => {
   const router = useRouter();
   const { user, isAuthenticated, isLoading, loginWithRedirect } = useAuth0();
@@ -18,5 +18,13 @@ export const ProtectedRoute = ({ children }) => {
     if (!isAuthenticated && router.pathname !== '/auth') handleLogin();
     else return children;
   }
-  return <div>Please wait while our awesome app is loading</div>;
+  return (
+    <div>
+      <AbsoluteCenter>
+        <VStack>
+          <Spinner boxSize={70} thickness="7px" speed="0.9s" color="purple" emptyColor="grey" />
+        </VStack>
+      </AbsoluteCenter>
+    </div>
+  );
 };
