c83cb6e973692ad1fe1079ebee9a23906a4bc1a4
Abhinav <abhinavmohanan17@gmail.com>
2024-02-15 12:00:07 +0530

    feat: Microservice and boilerplate for mailer

---
+++ apps/core-mailer/src/index.ts
@@ -0,0 +1,47 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import { sendMail } from './controller/mail';
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+dotenv.config();
+
+const port = process.env.PORT || 80;
+
+const app = express();
+app.use(
+  cors({
+    origin: '*',
+  }),
+);
+app.use(bodyParser.json());
+
+app.get('/health', (req: Request, res: Response) => {
+  const healthcheck: any = {
+    resource: 'Techno Mailer Server',
+    uptime: process.uptime(),
+    responseTime: process.hrtime(),
+    message: 'OK',
+    timestamp: Date.now(),
+  };
+  try {
+    res.send(healthcheck);
+  } catch (error) {
+    healthcheck.message = error;
+    res.status(503).send();
+  }
+});
+
+app.post('/send-mail', async (req: Request, res: Response) => {
+  const { email, subject, text } = req.body;
+  try {
+    await sendMail(email, subject, text);
+    res.status(200).send('Email sent successfully');
+  } catch (error) {
+    res.status(500).send('Email failed to send');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
