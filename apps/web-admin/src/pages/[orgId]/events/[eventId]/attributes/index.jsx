d6c1b2c8eeb5ed7a3ebfb745040b023e8d0100a3
Diya Therese Shibu <diyathereseshibu@gmail.com>
2024-09-18 22:02:40 +0530

    modal

--- apps/web-admin/src/pages/[orgId]/events/[eventId]/attributes/index.jsx
+++ apps/web-admin/src/pages/[orgId]/events/[eventId]/attributes/index.jsx
@@ -1,4 +1,4 @@
-import { useRouter } from 'next/router';
+/*import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
 import { Button } from '@chakra-ui/react';
@@ -73,6 +73,95 @@ export default function Attributes() {
           router.push(`/${orgId}/events/${eventId}/attributes/${row.id}`);
         }}
       />
+    </DashboardLayout>
+  );
+}*/
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import {
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+} from '@chakra-ui/react';
+import DashboardLayout from '@/layouts/DashboardLayout';
+import { useFetch } from '@/hooks/useFetch';
+import { useAlert } from '@/hooks/useAlert';
+import DataDisplay from '@/components/DataDisplay';
+import NewAttributeForm from './new';
+
+const columns = [
+  { field: 'name', headerName: 'Name', width: 200 },
+  {
+    field: 'numberOfParticipantsWithAttributeAssigned',
+    headerName: 'No of Participants Assigned',
+    width: 200,
+  },
+];
+
+export default function Attributes() {
+  const router = useRouter();
+  const { orgId, eventId } = router.query;
+  const showAlert = useAlert();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { loading, get } = useFetch();
+
+  const [attributes, setAttributes] = useState([]);
+
+  useEffect(() => {
+    const fetchAttributes = async () => {
+      const { data, status } = await get(
+        `/core/organizations/${orgId}/events/${eventId}/attributes`,
+      );
+      if (status === 200) {
+        setAttributes(data.attributes || []);
+      } else {
+        showAlert({
+          title: 'Error',
+          description: data.error,
+          status: 'error',
+        });
+      }
+    };
+    fetchAttributes();
+  }, []);
+
+  return (
+    <DashboardLayout
+      pageTitle="Attributes"
+      previousPage={`/organizations/${orgId}/events/${eventId}`}
+      headerButton={
+        <>
+          <Button onClick={onOpen} isLoading={loading}>
+            Add Attribute
+          </Button>
+        </>
+      }
+      debugInfo={JSON.stringify(attributes)}
+    >
+      <DataDisplay
+        loading={loading}
+        columns={columns}
+        rows={attributes}
+        onRowClick={(row) => {
+          router.push(`/${orgId}/events/${eventId}/attributes/${row.id}`);
+        }}
+      />
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Add Attribute</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <NewAttributeForm onClose={onClose} />
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </DashboardLayout>
   );
 }
--- apps/web-admin/src/pages/[orgId]/events/new/index.jsx
+++ apps/web-admin/src/pages/[orgId]/events/new/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+/*import { useState } from 'react';
 import { useRouter } from 'next/router';
 
 import { Button, FormControl, FormLabel, Input } from '@chakra-ui/react';
@@ -61,5 +61,60 @@ export default function NewEvent() {
         </Button>
       </form>
     </DashboardLayout>
+  );
+}
+*/
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { Button, FormControl, FormLabel, Input } from '@chakra-ui/react';
+import { useAlert } from '@/hooks/useAlert';
+import { useFetch } from '@/hooks/useFetch';
+
+export default function NewEventForm({ onClose }) {
+  const { loading, post } = useFetch();
+  const showAlert = useAlert();
+  const router = useRouter();
+  const { orgId } = router.query;
+
+  const [name, setName] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { data, status } = await post(`/core/organizations/${orgId}/events`, {}, { name });
+    if (status === 200) {
+      showAlert({
+        title: 'Success',
+        description: 'Event has been created successfully.',
+        status: 'success',
+      });
+      onClose();
+      router.push(`/${orgId}/events`);
+    } else {
+      showAlert({
+        title: 'Error',
+        description: data.error,
+        status: 'error',
+      });
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <FormControl isRequired my={4}>
+        <FormLabel>Name</FormLabel>
+        <Input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} />
+      </FormControl>
+
+      <Button
+        type="submit"
+        width="100%"
+        my="4"
+        isLoading={loading}
+        loadingText="Please Wait"
+        colorScheme="teal"
+      >
+        Add
+      </Button>
+    </form>
   );
 }
