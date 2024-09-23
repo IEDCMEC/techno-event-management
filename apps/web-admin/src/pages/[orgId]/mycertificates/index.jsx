afd7092e489c41fec9c3cacfb911b26e11f41878
Subramani E <mdl21ec119@mec.ac.in>
2024-09-24 00:10:16 +0530

    Fixing build errors 6

--- apps/web-admin/src/pages/[orgId]/mycertificates/index.jsx
+++ apps/web-admin/src/pages/[orgId]/mycertificates/index.jsx
@@ -3,6 +3,8 @@ import { useRouter } from 'next/router';
 import DashboardLayout from '@/layouts/DashboardLayout';
 // import CertifcateUploadBox from '@/components/CertificateUploadBox';
 // import ComingSoon from '@/components/ComingSoon';
+import dynamic from 'next/dynamic';
+
 const CertifcateUploadBox = dynamic(() => import('@/components/CertificateUploadBox'), {
   ssr: false,
 });
