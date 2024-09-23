774a5811b1c99d9751160947c05197efb068b2f1
Amal Varghese <145193788+codewizard-2004@users.noreply.github.com>
2024-09-23 18:32:23 +0530

    initialized the canvas in certifcate (#448)

--- apps/web-admin/src/pages/[orgId]/mycertificates/index.jsx
+++ apps/web-admin/src/pages/[orgId]/mycertificates/index.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import DashboardLayout from '@/layouts/DashboardLayout';
 import ComingSoon from '@/components/ComingSoon';
+import CertifcateUploadBox from '@/components/CertificateUploadBox';
 
 const MyCertificates = () => {
   const router = useRouter();
   const { orgId } = router.query;
   return (
     <DashboardLayout pageTitle="My Certificates" previousPage={`${orgId}`}>
-      <ComingSoon />
+      {/* <ComingSoon /> */}
+      <CertifcateUploadBox />
     </DashboardLayout>
   );
 };
