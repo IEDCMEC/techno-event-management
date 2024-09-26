bd78c38bfd7657762ceced5ced59db18ea7518bb
sreehhari <sreehari9300@gmail.com>
2024-09-26 23:31:21 +0530

    organization settings only renders for ADMINs

--- apps/web-admin/src/components/Sidebar.jsx
+++ apps/web-admin/src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import {
   Box,
@@ -26,6 +26,9 @@ const Sidebar = ({ isOpen, onClose }) => {
   const [loading, setLoading] = useState(false);
   const { logout } = useAuth0();
   const [isMobile] = useMediaQuery('(max-width: 768px)');
+  const { accountDetails } = useContext(account);
+  const isAdmin = accountDetails.role === 'ADMIN';
+  // const isUser = accountDetails.role === 'USER';
 
   const router = useRouter();
   const { orgId } = router.query;
@@ -61,15 +64,18 @@ const Sidebar = ({ isOpen, onClose }) => {
           <SidebarContents />
 
           <Box flex="1"></Box>
-          <Button
-            onClick={() => {
-              router.push(`/${orgId}/settings`);
-            }}
-            isLoading={loading}
-            width="100%"
-          >
-            Organization Settings
-          </Button>
+          {isAdmin && (
+            <Button
+              onClick={() => {
+                router.push(`/${orgId}/settings`);
+              }}
+              isLoading={loading}
+              width="100%"
+            >
+              Organization Settings
+            </Button>
+          )}
+
           <Box paddingY={4}>
             <Button
               onClick={handleLogout}
@@ -97,15 +103,18 @@ const Sidebar = ({ isOpen, onClose }) => {
                   <SidebarContents />
 
                   <Box flex="1"></Box>
-                  <Button
-                    onClick={() => {
-                      router.push(`/${orgId}/settings`);
-                    }}
-                    isLoading={loading}
-                    width="100%"
-                  >
-                    Organization Settings
-                  </Button>
+                  {isAdmin && (
+                    <Button
+                      onClick={() => {
+                        router.push(`/${orgId}/settings`);
+                      }}
+                      isLoading={loading}
+                      width="100%"
+                    >
+                      Organization Settings
+                    </Button>
+                  )}
+
                   <Box paddingY={4}>
                     <Button
                       onClick={handleLogout}
@@ -138,6 +147,7 @@ const SidebarContents = () => {
     { label: 'My Certificates', path: `/${orgId}/mycertificates`, icon: <PiCertificate /> },
     { label: 'Email Settings', path: `/${orgId}/emailsettings`, icon: <MdOutlineEmail /> },
     ...(isUser ? [{ label: 'Settings', path: `/settings`, icon: <MdOutlineSettings /> }] : []),
+    // ...(isAdmin ? [{label:'Organizaion Settings',path:'/${orgId}/settings',icon}]: []),
   ];
 
   return (
