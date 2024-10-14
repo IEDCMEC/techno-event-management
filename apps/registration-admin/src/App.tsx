8f38b5baf77147d1a8bef884293b037cac460902
midhununni457 <midhununni457@gmail.com>
2024-10-15 00:31:27 +0530

    added chakra-ui

--- apps/registration-admin/src/App.tsx
+++ apps/registration-admin/src/App.tsx
@@ -1,8 +1,12 @@
 // import React from 'react'
+import {ChakraProvider} from "@chakra-ui/react"
+import Form from "./Form"
 
 const App = () => {
   return (
-    <div>App</div>
+    <ChakraProvider>
+      <Form/>
+    </ChakraProvider>
   )
 }
 
---
+++ apps/registration-admin/src/Form.tsx
@@ -0,0 +1,12 @@
+import { Input } from '@chakra-ui/react';
+
+const Form = () => {
+  return (
+    <div>
+      <Input placeholder="Name" />
+      <Input placeholder="Phone" />
+    </div>
+  );
+};
+
+export default Form;
