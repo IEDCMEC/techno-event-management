1990d2655afd2e460e240609eef0bc6251da1df9
Midhun Unni <96683120+midhununni457@users.noreply.github.com>
2024-11-15 22:16:50 +0530

    feat: added user registration page (registration-admin) (#507)
    * trial push
    
    * added button
    
    * modal added
    
    * added modal form fields
    
    * removed unnecessary code
    
    * added chakra-ui
    
    * form field display (error not fixed)
    
    * feat: Added participand routes and hooks for registration-admin
    
    * Did something
    
    * proper post reuest set
    
    * feat: added route for participant form submission in registration-admin
    
    * registration completed
    
    * Registration complete page added
    
    * added cookie to check registration
    
    * added endOfLine to prettier
    
    * removed eol from prettier
    
    ---------
    
    Co-authored-by: aaron-6960 <138325645+aaron-6960@users.noreply.github.com>
    Co-authored-by: aaron-6960 <aaroongeorge2005@gmail.com>
    Co-authored-by: Subramani E <93091455+subru-37@users.noreply.github.com>

---
+++ apps/registration-admin/src/pages/Form.tsx
@@ -0,0 +1,87 @@
+import { FormControl, FormLabel, Input, Button } from '@chakra-ui/react';
+import { useFetch } from '../hooks/useFetch';
+import { useAlert } from '../hooks/useAlert';
+import { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+
+const Form = () => {
+  const [attributes, setAttributes] = useState<any[]>([]);
+  const [formData, setFormData] = useState<{ [key: string]: string }>({});
+  const { eventID, orgID } = useParams<{ eventID: string; orgID: string }>();
+  const { get, post } = useFetch();
+  const showAlert = useAlert();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchEventAttributes = async () => {
+      const checkResponse = await get(`/registration/${orgID}/event/${eventID}/verify`);
+      if (checkResponse?.status === 200) {
+        const response = await get(`/registration/${orgID}/event/${eventID}/attributes`);
+        if (response?.status === 200) {
+          setAttributes(response?.data.attributes || []);
+        } else {
+          showAlert({
+            title: 'Error',
+            description: response?.data.error,
+            status: 'error',
+          });
+        }
+      } else {
+        navigate('/');
+      }
+    };
+
+    const hasRegistered = document.cookie.includes(`registered_${eventID}=true`);
+    if (hasRegistered) navigate('/already-registered');
+
+    fetchEventAttributes();
+  }, []);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = event.target;
+    setFormData((prevData) => ({ ...prevData, [id]: value }));
+  };
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log(formData);
+    const response = await post(`/registration/${orgID}/event/${eventID}/submit`, formData);
+    if (response?.status === 200) {
+      document.cookie = `registered_${eventID}=true; path=/; max-age=${60 * 60 * 24 * 30};`;
+      showAlert({
+        title: 'Success',
+        description: 'Form submitted successfully!',
+        status: 'success',
+      });
+      navigate('/');
+    } else {
+      showAlert({
+        title: 'Error',
+        description: response?.data.error || 'Submission failed.',
+        status: 'error',
+      });
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {attributes.map((attr: any, index: number) => (
+        <FormControl mb={5} key={index} isRequired>
+          <FormLabel>{attr.name}</FormLabel>
+          <Input
+            id={attr.id}
+            name={attr.name} // Use the attribute name as the input name
+            placeholder={`${attr.name}`}
+            onChange={handleInputChange} // Handle input change
+            required // Mark as required
+          />
+        </FormControl>
+      ))}
+      <Button type="submit" colorScheme="blue">
+        Submit
+      </Button>
+    </form>
+  );
+};
+
+export default Form;
---
+++ apps/web-admin/src/hooks/useAlert.d.ts
@@ -0,0 +1,8 @@
+interface AlertOptions {
+  title: string;
+  description?: string;
+  status: 'success' | 'error' | 'warning' | 'info';
+  duration?: number;
+}
+
+export declare const useAlert: () => (options: AlertOptions) => void;
