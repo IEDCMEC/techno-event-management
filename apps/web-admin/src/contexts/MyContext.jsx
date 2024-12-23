61826e8b44a3bad0fae4ecf0f4a2b2f11c592e5c
Ajay Krishna D <145097082+Ajay-Krishna00@users.noreply.github.com>
2024-12-23 13:30:47 +0530

    feat: Created New UI for "Settings" page which is compatible with dark mode and is responsive (#616)
    * feat:added back and frontend for submitting Organisatio details
    
    * fix: removed repeating line
    
    * Created new Settings UI and edit modal
    
    * Resolved some errors
    
    * modified ui of location btn

--- apps/web-admin/src/contexts/MyContext.jsx
+++ apps/web-admin/src/contexts/MyContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect } from 'react';
+import { React, createContext, useState, useEffect } from 'react';
 import { useFetch } from '@/hooks/useFetch';
 import { useAlert } from '@/hooks/useAlert';
 import { useAuth0 } from '@auth0/auth0-react';
