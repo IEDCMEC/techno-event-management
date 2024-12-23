bba9c77255a60ed3d4746aa2e3bb333a6d5d23ad
Ajay Krishna D <145097082+Ajay-Krishna00@users.noreply.github.com>
2024-12-23 16:46:03 +0530

    Updated Settings UI (#618)
    * feat:added back and frontend for submitting Organisatio details
    
    * fix: removed repeating line
    
    * Created new Settings UI and edit modal
    
    * Resolved some errors
    
    * modified ui of location btn
    
    * done some minor UI changes again

--- apps/web-admin/src/layouts/DashboardLayout.jsx
+++ apps/web-admin/src/layouts/DashboardLayout.jsx
@@ -324,8 +324,6 @@ export default function DashboardLayout({ headerButton, children }) {
             </Flex>
           </Flex>
         </Flex>
-        <OrganizationSettingsModal isOpen={isOpen} onClose={onClose} />{' '}
-        {/* Organization Settings modal */}
       </Flex>
     );
   } else {
