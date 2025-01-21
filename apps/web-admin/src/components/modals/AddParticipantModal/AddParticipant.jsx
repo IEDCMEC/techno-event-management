fe981fdb28eedc4ec4739e9ac3ad7666098c9fc6
Subramani E <mdl21ec119@mec.ac.in>
2025-01-21 14:36:58 +0530

    prevention of empty participant insertion

--- apps/web-admin/src/components/modals/AddParticipantModal/AddParticipant.jsx
+++ apps/web-admin/src/components/modals/AddParticipantModal/AddParticipant.jsx
@@ -32,82 +32,90 @@ const AddParticipant = ({ isOpen, onClose, formData, handleInputChange, handleSu
           <StyledText color="black">Add Participant</StyledText>
         </ModalHeader>
         <ModalCloseButton />
-        <ModalBody backgroundColor={colorMode === 'light' ? '#EEEFFF' : '#101116'}>
-          <FormControl mb={4} isRequired>
-            <FormLabel fontFamily={inter.style.fontFamily}>First Name</FormLabel>
-            <Input
-              backgroundColor={colorMode === 'light' ? '#04050B12' : '#FBFBFE12'}
-              name="firstName"
-              value={formData.firstName}
-              onChange={handleInputChange}
-              placeholder="First Name"
-            />
-          </FormControl>
-          <FormControl mb={4} isRequired>
-            <FormLabel fontFamily={inter.style.fontFamily}>Last Name</FormLabel>
-            <Input
-              backgroundColor={colorMode === 'light' ? '#04050B12' : '#FBFBFE12'}
-              name="lastName"
-              value={formData.lastName}
-              onChange={handleInputChange}
-              placeholder="Last Name"
-            />
-          </FormControl>
-          <FormControl mb={4} isRequired>
-            <FormLabel fontFamily={inter.style.fontFamily}>Email</FormLabel>
-            <Input
-              backgroundColor={colorMode === 'light' ? '#04050B12' : '#FBFBFE12'}
-              name="email"
-              value={formData.email}
-              onChange={handleInputChange}
-              placeholder="Email"
-            />
-          </FormControl>
-          <FormControl mb={4} isRequired>
-            <FormLabel fontFamily={inter.style.fontFamily}>Phone</FormLabel>
-            <Input
-              backgroundColor={colorMode === 'light' ? '#04050B12' : '#FBFBFE12'}
-              name="phone"
-              value={formData.phone}
-              onChange={handleInputChange}
-              placeholder="Phone"
-            />
-          </FormControl>
-          <FormControl mb={4} isRequired>
-            <FormLabel fontFamily={inter.style.fontFamily}>Check In Key</FormLabel>
-            <Input
-              backgroundColor={colorMode === 'light' ? '#04050B12' : '#FBFBFE12'}
-              name="checkInKey"
-              value={formData.checkInKey}
-              onChange={handleInputChange}
-              placeholder="Check In Key"
-            />
-          </FormControl>
-        </ModalBody>
-        <ModalFooter
-          backgroundColor={colorMode === 'light' ? '#EEEFFF' : '#101116'}
-          borderBottomRadius="10px"
-          pt={0}
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit();
+          }}
         >
-          <Button
-            width="100%"
-            colorScheme="blue"
-            onClick={handleSubmit}
-            backgroundColor="#AFB4E9"
-            color="black"
-            _hover={{ backgroundColor: '#D0D6F6 ' }}
+          <ModalBody backgroundColor={colorMode === 'light' ? '#EEEFFF' : '#101116'}>
+            <FormControl mb={4} isRequired>
+              <FormLabel fontFamily={inter.style.fontFamily}>First Name</FormLabel>
+              <Input
+                backgroundColor={colorMode === 'light' ? '#04050B12' : '#FBFBFE12'}
+                name="firstName"
+                value={formData.firstName}
+                onChange={handleInputChange}
+                placeholder="First Name"
+              />
+            </FormControl>
+            <FormControl mb={4} isRequired>
+              <FormLabel fontFamily={inter.style.fontFamily}>Last Name</FormLabel>
+              <Input
+                backgroundColor={colorMode === 'light' ? '#04050B12' : '#FBFBFE12'}
+                name="lastName"
+                value={formData.lastName}
+                onChange={handleInputChange}
+                placeholder="Last Name"
+              />
+            </FormControl>
+            <FormControl mb={4} isRequired>
+              <FormLabel fontFamily={inter.style.fontFamily}>Email</FormLabel>
+              <Input
+                backgroundColor={colorMode === 'light' ? '#04050B12' : '#FBFBFE12'}
+                name="email"
+                value={formData.email}
+                onChange={handleInputChange}
+                placeholder="Email"
+              />
+            </FormControl>
+            <FormControl mb={4} isRequired>
+              <FormLabel fontFamily={inter.style.fontFamily}>Phone</FormLabel>
+              <Input
+                backgroundColor={colorMode === 'light' ? '#04050B12' : '#FBFBFE12'}
+                name="phone"
+                value={formData.phone}
+                onChange={handleInputChange}
+                placeholder="Phone"
+              />
+            </FormControl>
+            <FormControl mb={4} isRequired>
+              <FormLabel fontFamily={inter.style.fontFamily}>Check In Key</FormLabel>
+              <Input
+                backgroundColor={colorMode === 'light' ? '#04050B12' : '#FBFBFE12'}
+                name="checkInKey"
+                value={formData.checkInKey}
+                onChange={handleInputChange}
+                placeholder="Check In Key"
+              />
+            </FormControl>
+          </ModalBody>
+          <ModalFooter
+            backgroundColor={colorMode === 'light' ? '#EEEFFF' : '#101116'}
+            borderBottomRadius="10px"
+            pt={0}
           >
-            <StyledText>Submit</StyledText>
-          </Button>
-          <Button
-            backgroundColor={colorMode === 'light' ? '#04050B12' : '#FBFBFE1212'}
-            width="100%"
-            onClick={onClose}
-            ml={3}
-          >
-            <StyledText>Cancel</StyledText>
-          </Button>
-        </ModalFooter>
+            <Button
+              width="100%"
+              colorScheme="blue"
+              // onClick={handleSubmit}
+              backgroundColor="#AFB4E9"
+              type="submit"
+              color="black"
+              _hover={{ backgroundColor: '#D0D6F6 ' }}
+            >
+              <StyledText>Submit</StyledText>
+            </Button>
+            <Button
+              backgroundColor={colorMode === 'light' ? '#04050B12' : '#FBFBFE1212'}
+              width="100%"
+              onClick={onClose}
+              ml={3}
+            >
+              <StyledText>Cancel</StyledText>
+            </Button>
+          </ModalFooter>
+        </form>
       </ModalContent>
     </Modal>
   );
